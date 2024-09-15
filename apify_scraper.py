41d419bec1d64dbde74c13b64d845321b2c43e93
alokranjan1234 <alok.ranjan.5991@gmail.com>
2024-09-15 22:49:16 +0530

    initial commit

---
+++ apify_scraper.py
@@ -0,0 +1,62 @@
+from apify_client import ApifyClient
+import json
+import os
+from dotenv import load_dotenv
+from scraper_configs import ACTOR_DICT
+from pymongo import MongoClient
+from datetime import datetime,timezone
+
+def run_scraper(site,scraper_actor, custom_params=None):
+    # Load environment variables
+    load_dotenv(override=True)
+
+    # Initialize the ApifyClient with your API token
+    client = ApifyClient(os.getenv('APIFY_API_TOKEN'))
+
+    # Get scraper configuration
+    scraper_config = ACTOR_DICT.get(site,{}).get(scraper_actor)
+    if not scraper_config:
+        raise ValueError(f"Scraper '{scraper_actor}' not found in configuration.")
+
+    # Merge default params with custom params
+    run_input = scraper_config['params'].copy()
+    if custom_params:
+        run_input.update(custom_params)
+
+    # Run the Actor
+    run = client.actor(scraper_config['name']).call(run_input=run_input)
+
+    try:
+        print(run['statusMessage'])
+    except:
+        pass
+    # Fetch and return Actor results from the run's dataset
+    return client.dataset(run["defaultDatasetId"]).iterate_items()
+
+def save_to_mongodb(data, site, scraper_actor):
+    # Connect to MongoDB
+    mongo_client = MongoClient(os.getenv('MONGODB_URI'))
+    db = mongo_client[os.getenv('MONGODB_DB_NAME')]
+    collection = db[site]
+
+    # Add timestamp to the data
+    data['timestamp'] = str(datetime.now(timezone.utc))
+
+    # Insert the data into MongoDB
+    data['actor'] = scraper_actor
+    result = collection.insert_one(data)
+    print(f"Inserted document with ID: {result.inserted_id}")
+
+if __name__ == "__main__":
+
+    # Example usage
+    site ="hotels"
+    scraper_actor = "jeremy_frost_hotels_com_scraper"
+    custom_params = {
+        "limit": 1000,
+        "location": "Spain",
+    }
+
+    results = run_scraper(site,scraper_actor, custom_params)
+    for item in results:
+        save_to_mongodb(item, site, scraper_actor)
---
+++ scraper_configs.py
@@ -0,0 +1,75 @@
+ACTOR_DICT={
+    "booking": {
+        "voyager_booking_scraper": {
+        "name": "voyager/booking-scraper",
+        "params" : {
+            "currency": "USD",
+            "language": "en-gb",
+            "maxItems": 10,
+            "minMaxPrice": "0-999999",
+            "minScore": "5",
+            "propertyType": "Hotels",
+            "search": "New York",
+            "sortBy": "distance_from_search",
+            "starsCountFilter": "any"
+        }
+        },
+        "voyager_fast_booking_scraper": {
+            "name":"voyager/fast-booking-scraper",
+            "params":{
+            "currency": "USD",
+            "flexWindow": "0",
+            "language": "en-gb",
+            "maxItems": 200,
+            "minMaxPrice": "5-999999",
+            "minScore": "5",
+            "propertyType": "Hotels",
+            "rooms": 1,
+            "search": "New York",
+            "sortBy": "distance_from_search",
+            "starsCountFilter": "any"
+        }
+        }
+
+        },
+    "expedia":{
+        "jupri_expedia_hotels" :{
+            "name": "jupri/expedia-hotels",
+            "params":{
+                "dev_dataset_clear": False,
+                "dev_no_strip": False,
+                "includes:amenities": False,
+                "includes:availability": False,
+                "includes:calendar": False,
+                "includes:description": False,
+                "includes:faq": False,
+                "includes:gallery": False,
+                "includes:landmarks": False,
+                "includes:location": False,
+                "includes:offers": False,
+                "includes:policies": False,
+                "includes:review": False,
+                "language": "EN_US",
+                "limit": 5,
+                "location": "New York",
+                "rewards.member_only": False,
+                "rewards.vip": False,
+                "site": "1",
+                "types": [
+                    "hotel"
+                ]
+            }   
+        }
+    },
+
+    "hotels":{
+        "jeremy_frost_hotels_com_scraper" : {
+        "name": "jeremy_frost/hotels-com-scraper",
+        "params": {
+            "max_depth": 500,
+            "min_stars": "0",
+            "place": "Spain"
+            }
+        }
+    }
+}
